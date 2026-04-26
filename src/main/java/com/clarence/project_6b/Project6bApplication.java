504f419f7dfbbf438bb916f445811326436b965f
Clarence <clarenceccesante@gmail.com>
2026-04-25 20:52:13 -0400

    Initial commit

---
+++ src/main/java/com/clarence/project_6b/Project6bApplication.java
@@ -0,0 +1,13 @@
+package com.clarence.project_6b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project6bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project6bApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clarence/project_6b/Project6bApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clarence.project_6b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project6bApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
